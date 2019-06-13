b162b8e571caeb54c42e6560de7d14ade0696e23
Daniel Schneider <daschne8@gmail.com>
2019-06-13 15:16:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ class BoardMember{
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
-class Ceo extends BoardMember{
-  hireEmployee(){
-    return "Welcome aboard!"
-  }
-}
+// class Ceo extends BoardMember{
+//   hireEmployee(){
+//     return "Welcome aboard!"
+//   }
+// }
